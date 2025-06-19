8115862645fc6df3496b03a0725c324df52be49c
Joao Velez <joao.velez2020@hotmail.com>
2025-06-19 11:31:48 +0100

    Created script to generate genesis Utxo for a script

--- freelance_marketplace/api/utils/blockchain/create_wallet_credentials.py
+++ freelance_marketplace/api/utils/blockchain/create_wallet_credentials.py
@@ -1,19 +1,22 @@
 import os
-
 from pycardano import PaymentSigningKey, PaymentVerificationKey
 
-# Path to save
-wallet_dir = os.path.expanduser("~/Documents/security/wallet_credentials")
-os.makedirs(wallet_dir, exist_ok=True)
+def generate_credentials():
+    # Path to save
+    wallet_dir = os.path.expanduser("~/Documents/security/wallet_credentials")
+    os.makedirs(wallet_dir, exist_ok=True)
 
-# Generate keys
-signing_key = PaymentSigningKey.generate()
-verification_key = PaymentVerificationKey.from_signing_key(signing_key)
+    # Generate keys
+    signing_key = PaymentSigningKey.generate()
+    verification_key = PaymentVerificationKey.from_signing_key(signing_key)
 
-# Write signing key
-with open(os.path.join(wallet_dir, "payment.skey"), "w") as f:
-    f.write(signing_key.to_cbor_hex())
+    # Write signing key
+    with open(os.path.join(wallet_dir, "payment.skey"), "w") as f:
+        f.write(signing_key.to_json())
 
-# Write verification key
-with open(os.path.join(wallet_dir, "payment.vkey"), "w") as f:
-    f.write(verification_key.to_cbor_hex())
+    # Write verification key
+    with open(os.path.join(wallet_dir, "payment.vkey"), "w") as f:
+        f.write(verification_key.to_json())
+
+if __name__ == "__main__":
+    generate_credentials()
