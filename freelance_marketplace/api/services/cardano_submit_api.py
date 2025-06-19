363bcf0225e97c83fa3062d377b92a9d3464d034
Joao Velez <joao.velez2020@hotmail.com>
2025-06-19 15:37:56 +0100

    Created logic to submit api and verify if tx is properly signed

--- freelance_marketplace/api/services/cardano_submit_api.py
+++ freelance_marketplace/api/services/cardano_submit_api.py
@@ -1,3 +1,33 @@
+from fastapi import HTTPException
+from pycardano import Transaction
+import requests
+from freelance_marketplace.core.config import settings
+
+
 class SubmitAPI:
     def __init__(self):
-        pass
+        self.submit_api_url = settings.blockchain.submit_api_url
+
+    async def is_transaction_signed(self, tx: Transaction) -> bool:
+        witnesses = tx.transaction_witness_set
+        if not witnesses or not witnesses.vkey_witnesses:
+            return False
+
+        return len(witnesses.vkey_witnesses) > 0
+
+    async def submit_transaction(self, tx: Transaction) -> bool:
+        try:
+            if not self.is_transaction_signed(tx=tx):
+                raise HTTPException(500, "Transaction was not signed.")
+
+            tx_bytes: bytes = tx.to_cbor()
+            headers = {"content-type": "application/cbor"}
+            response = requests.post(self.submit_api_url, data=tx_bytes, headers=headers)
+
+            if response.status_code != 200:
+                raise HTTPException(500, "Failed to submit transaction")
+            else:
+                return True
+        except Exception as e:
+            print(f"Failed to submit transaction: {e}")
+            raise e
